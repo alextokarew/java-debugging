fbd0de29ee53f3a35cccc24dea3431773497a82d
alextokarew <alex.tokarew@gmail.com>
2026-01-19 12:37:49 +0300

    Async stack traces example

--- app/src/main/java/com/github/alextokarew/javadebugging/multithreaded/AsyncExample.java
+++ app/src/main/java/com/github/alextokarew/javadebugging/multithreaded/AsyncExample.java
@@ -1,25 +1,40 @@
 package com.github.alextokarew.javadebugging.multithreaded;
 
+
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class AsyncExample {
-    static void main() throws Exception {
-//        var pool = ForkJoinPool.commonPool();
-
-        var f = CompletableFuture.supplyAsync(() -> {
-            IO.println("1. Current thread: " + Thread.currentThread().getName());
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            return "Done";
-        }).thenApplyAsync((s) -> {
-            IO.println("2. Current thread: " + Thread.currentThread().getName());
-            return "Well " + s;
+    private static CompletableFuture<User> getUserByName(String name) {
+        return CompletableFuture.supplyAsync(() -> {
+            System.out.println("UserService: fetching user by name " + name);
+            try { Thread.sleep(800); } catch (InterruptedException ignored) {}
+            return new User(1, name);
         });
-
-        System.out.println(f.get());
     }
+
+    private static CompletableFuture<List<Order>> getOrders(int userId) {
+        return CompletableFuture.supplyAsync(() -> {
+            System.out.println("OrderService: fetching orders for user " + userId);
+            try { Thread.sleep(1000); } catch (InterruptedException ignored) {}
+            if (userId == 1) throw new RuntimeException("BOOM");
+            return Arrays.asList(
+                    new Order(101, userId, "Book"),
+                    new Order(102, userId, "Laptop")
+            );
+        });
+    }
+
+    static void main(String[] args) throws ExecutionException, InterruptedException {
+        CompletableFuture<User> userFuture = getUserByName(args[0]);
+        CompletableFuture<List<Order>> ordersFuture = userFuture.thenCompose(user -> getOrders(user.id));
+        IO.println("Orders for user: " + ordersFuture.get());
+    }
+
+    record User(int id, String name) {}
+    record Order(int id, int userId, String item) {}
 }
